f0a8d2394fbb6d775df7a48dce2b23acec041eff
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2018-01-09 10:33:27 +1100

    Add scheduledtask

--- src/main/java/sixkiller/sample/JoggingApplication.java
+++ src/main/java/sixkiller/sample/JoggingApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by ala on 9.5.16.
  */
 
 @SpringBootApplication
+@EnableScheduling
 public class JoggingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
