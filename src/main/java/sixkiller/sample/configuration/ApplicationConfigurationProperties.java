c6b0f061ff6a45005e789f4663fb7d5610a3aa83
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2018-01-09 09:51:33 +1100

    Add ADMIN USER

--- src/main/java/sixkiller/sample/configuration/ApplicationConfigurationProperties.java
+++ src/main/java/sixkiller/sample/configuration/ApplicationConfigurationProperties.java
@@ -4,9 +4,34 @@ package sixkiller.sample.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import sixkiller.sample.domain.User;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
 @Component
 @ConfigurationProperties(prefix="appconfig")
 public class ApplicationConfigurationProperties {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @PostConstruct
+    public void init() throws IOException {
+        User admin = new User();
+        String[] roles = {"ADMIN", "USER"};
+        admin.setUserName("nmy3");
+        admin.setPassword(passwordEncoder.encode("password"));
+        admin.setRoles(roles);
+        mongoTemplate.save(admin);
+    }
 
     private String[] defaultUserRoles;
 
